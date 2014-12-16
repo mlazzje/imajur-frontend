69fcfc0c6ebab8b71e18c8fdfe84ba7ab1872a41
mlazzje <mlazzje@gmail.com>
2014-12-16 17:49:11 +0100

    suppression playlist

--- www/js/app.js
+++ www/js/app.js
@@ -38,31 +38,11 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       }
     }
   })
-
   .state('app.browse', {
     url: "/browse",
     views: {
       'menuContent': {
         templateUrl: "templates/browse.html"
-      }
-    }
-  })
-    .state('app.playlists', {
-      url: "/playlists",
-      views: {
-        'menuContent': {
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-  .state('app.single', {
-    url: "/playlists/:playlistId",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/playlist.html",
-        controller: 'PlaylistCtrl'
       }
     }
   })
